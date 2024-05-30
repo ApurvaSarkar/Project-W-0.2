2d4f5739da852e266f49122d7ec8fb278538731e
apurva <apurvasarkar@gmail.com>
2024-05-30 21:07:02 +0530

    #

--- run Docker DB.sh
+++ run Docker DB.sh
@@ -1,5 +1,7 @@
 docker run -p 5532:5432 -d -e POSTGRES_DB=ai -e POSTGRES_USER=ai -e POSTGRES_PASSWORD=ai -e PGDATA=/var/lib/postgresql/data/pgdata -v pgvolume:/var/lib/postgresql/data  --name pgvector phidata/pgvector:16
 
+docker ps
+
 docker exec -it 24bf5356c5aa psql -U ai
 
 \list
